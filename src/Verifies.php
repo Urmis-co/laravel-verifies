31af987852c840140ea90c689c9905414bf7e64e
bluemmb <bluemmb22@gmail.com>
2019-08-14 20:06:10 +0430

    Add table name to model

--- src/Verifies.php
+++ src/Verifies.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Verifies extends Model
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'verifies';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
